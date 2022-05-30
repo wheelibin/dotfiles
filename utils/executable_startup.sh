d0476fcbe09f34f086a1bc5ed11262f575e63576
wheelibin <wheelibin@gmail.com>
2022-05-30 22:09:24 +0100

    utils folder

---
+++ utils/executable_startup.sh
@@ -0,0 +1,8 @@
+# enable window transparency
+picom --config ~/.config/picom/picom.conf -b
+
+# caps lock as escape and ctrl
+xcape -e 'Caps_Lock=Escape' -t 100
+
+# set random wallpaper
+~/utils/wallpaper-change.sh
