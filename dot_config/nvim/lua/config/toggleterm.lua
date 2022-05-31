e65f72e9e0876bdc9d462829d9019a48a29be9af
Jon Wheeler <jon.wheeler@astrazeneca.com>
2022-05-31 16:55:05 +0100

    Update dot_config/nvim/lua/config/toggleterm.lua

--- dot_config/nvim/lua/config/toggleterm.lua
+++ dot_config/nvim/lua/config/toggleterm.lua
@@ -1,1 +1,10 @@
 require('toggleterm').setup()
+
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+function Lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua Lazygit_toggle()<CR>", {noremap = true, silent = true})
