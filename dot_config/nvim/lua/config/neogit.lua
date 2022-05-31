d679ba43669013ac833a87b41b894d32ddd79a69
Jon Wheeler <jon.wheeler@astrazeneca.com>
2022-05-31 16:55:35 +0100

    Update dot_config/nvim/lua/config/neogit.lua

--- dot_config/nvim/lua/config/neogit.lua
+++ dot_config/nvim/lua/config/neogit.lua
@@ -3,4 +3,4 @@ require('neogit').setup()
 local map = vim.api.nvim_set_keymap
 local options = { noremap = true }
 
-map('n', '<Leader>g', '<cmd>lua require("neogit").open({ kind = "vsplit" })<cr>', options)
+-- map('n', '<Leader>g', '<cmd>lua require("neogit").open({ kind = "vsplit" })<cr>', options)
