182a4646699b49fe277fd164e402f0bdb1ecc6c7
wheelibin <wheelibin@gmail.com>
2022-06-01 16:41:26 +0100

    Update dot_config/nvim/lua/config/null-ls.lua

--- dot_config/nvim/lua/config/null-ls.lua
+++ dot_config/nvim/lua/config/null-ls.lua
@@ -1,5 +1,6 @@
 local null_ls = require('null-ls')
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
 null_ls.setup({
     sources = {
       null_ls.builtins.code_actions.eslint_d,
