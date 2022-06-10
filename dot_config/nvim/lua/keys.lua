ed8674211bdff97d73a1481219323ddbba83cd32
Jon Wheeler <jon.wheeler@astrazeneca.com>
2022-06-10 07:29:48 +0100

    Update dot_config/nvim/lua/keys.lua

--- dot_config/nvim/lua/keys.lua
+++ dot_config/nvim/lua/keys.lua
@@ -10,13 +10,12 @@ map("n", "<M-j>", ":wincmd j<CR>")
 map("n", "<M-k>", ":wincmd k<CR>")
 map("n", "<M-l>", ":wincmd l<CR>")
 map("n", "<M-r>", ":wincmd r<CR>")
-map("n", "<M-r>", ":wincmd r<CR>")
 map("n", "<M-q>", ":wincmd q<CR>")
 
 --- faster window creation
+map("n", "<M-S-h>", ":wincmd v<CR>:wincmd h<CR>")
 map("n", "<M-S-j>", ":wincmd s<CR>")
 map("n", "<M-S-k>", ":wincmd s<CR>:wincmd k<CR>")
-map("n", "<M-S-h>", ":wincmd v<CR>:wincmd h<CR>")
 map("n", "<M-S-l>", ":wincmd v<CR>")
 
 --- copy paste to system clipboard
@@ -24,7 +23,7 @@ map("v", "<Leader>y", '"+y')
 map("n", "<Leader>p", '"+p')
 map("n", "<Leader>P", '"+P')
 
---- move line in normal, instert and visual mode
+--- move line in normal, insert and visual mode
 map("n", "<C-j>", ":m .+1<CR>==")
 map("n", "<C-k>", ":m .-2<CR>==")
 map("i", "<C-j>", "<Esc>:m .+1<CR>==gi")
