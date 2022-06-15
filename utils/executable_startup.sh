dc52bed23540711f2807a191bc1c497efa721440
wheelibin <wheelibin@gmail.com>
2022-06-15 16:24:08 +0100

    Update utils/executable_startup.sh

--- utils/executable_startup.sh
+++ utils/executable_startup.sh
@@ -2,7 +2,7 @@
 picom --config ~/.config/picom/picom.conf -b
 
 # caps lock as escape and ctrl
-xcape -e 'Caps_Lock=Escape' -t 100
+xcape -e '#66=Escape' -t 100
 
 # set random wallpaper
 ~/utils/wallpaper-change.sh
