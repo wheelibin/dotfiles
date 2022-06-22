fad85814f4ab43aab9d896cbea8f700569a4d760
wheelibin <wheelibin@gmail.com>
2022-06-22 17:57:29 +0100

    Update dot_config/nvim/lua/config/lualine.lua

--- dot_config/nvim/lua/config/lualine.lua
+++ dot_config/nvim/lua/config/lualine.lua
@@ -1,5 +1,21 @@
 require('lualine').setup({
   options = {
     theme = 'nord'
-  }
+  },
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        file_status = true,
+        path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
+      }
+    }
+  },
+  tabline = {
+    lualine_z = {
+      {
+        'buffers',
+      }
+    },
+  },
 })
