abf5a90dda44e1ff996097f30ab9069d5cac448b
Jon Wheeler <jon.wheeler@astrazeneca.com>
2022-06-23 18:09:57 +0100

    Update dot_config/nvim/lua/config/lualine.lua

--- dot_config/nvim/lua/config/lualine.lua
+++ dot_config/nvim/lua/config/lualine.lua
@@ -1,3 +1,20 @@
+-- set up git blame to work with lualine
+local git_blame = require('gitblame')
+vim.g.gitblame_enable = 1
+vim.g.gitblame_display_virtual_text = 0
+vim.g.gitblame_date_format = '%r'
+
+-- if _G.Tabline_timer == nil then
+--   _G.Tabline_timer = vim.loop.new_timer()
+-- else
+--   _G.Tabline_timer:stop()
+-- end
+-- _G.Tabline_timer:start(0,             -- never timeout
+--                        1000,          -- repeat every 1000 ms
+--                        vim.schedule_wrap(function() -- updater function
+--                                             vim.api.nvim_command('redrawtabline')
+--                                          end))
+
 require('lualine').setup({
   options = {
     theme = 'nord'
@@ -5,10 +22,22 @@ require('lualine').setup({
   sections = {
     lualine_c = {
       {
+        git_blame.get_current_blame_text,
+        cond = git_blame.is_blame_text_available,
+      }
+    },
+    lualine_x = {'encoding', 'filetype'},
+},
+  tabline = {
+    lualine_a = {
+      {
         'filename',
         file_status = true,
         path = 1 -- 0 = just filename, 1 = relative path, 2 = absolute path
       }
+    },
+    lualine_z = {
+      'buffers'
     }
-  },
+  }
 })
