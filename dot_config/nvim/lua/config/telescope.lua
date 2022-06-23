9f5c6c4f4ab3be628be26abfe47ff925574c7bac
Jon Wheeler <jon.wheeler@astrazeneca.com>
2022-06-23 18:10:30 +0100

    Update dot_config/nvim/lua/config/telescope.lua

--- dot_config/nvim/lua/config/telescope.lua
+++ dot_config/nvim/lua/config/telescope.lua
@@ -3,6 +3,7 @@ local options = { noremap = true }
 
 local telescope = require('telescope')
 telescope.setup()
+
 telescope.load_extension('fzf')
 
 -- find shortcuts
@@ -10,3 +11,5 @@ map('n', '<Leader>ff', '<cmd>lua require("telescope.builtin").find_files()<cr>',
 map('n', '<Leader>fg', '<cmd>lua require("telescope.builtin").live_grep()<cr>', options)
 map('n', '<Leader>fb', '<cmd>lua require("telescope.builtin").buffers()<cr>', options)
 map('n', '<Leader>fh', '<cmd>lua require("telescope.builtin").help_tags()<cr>', options)
+map('n', '<Leader>fr', '<cmd>lua require("telescope.builtin").lsp_references({layout_strategy="vertical"})<cr>', options)
+map('n', '<Leader>fd', '<cmd>lua require("telescope.builtin").diagnostics({layout_strategy="vertical"})<cr>', options)
