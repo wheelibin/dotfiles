56fec9df01e5b86bb9c7a38199cfdef30567e04d
Jon Wheeler <jon.wheeler@astrazeneca.com>
2022-06-23 07:34:30 +0100

    Update dot_config/nvim/lua/config/nvim-test.lua

--- dot_config/nvim/lua/config/nvim-test.lua
+++ dot_config/nvim/lua/config/nvim-test.lua
@@ -2,7 +2,7 @@ require('nvim-test').setup()
 
  require('nvim-test.runners.jest'):setup {
     command = "node_modules/.bin/jest",                                       -- a command to run the test runner
-    args = { "--collectCoverage=false" },                                       -- default arguments
+    args = { "--collectCoverage=false --env=jest-environment-jsdom" },                                       -- default arguments
     -- env = { CUSTOM_VAR = 'value' },                                             -- custom environment variables
     file_pattern = "\\v(__tests__/.*|(spec|test))\\.(js|jsx|coffee|ts|tsx)$",   -- determine whether a file is a testfile
     find_files = { "{name}.test.{ext}", "{name}.spec.{ext}" },                  -- find testfile for a file
