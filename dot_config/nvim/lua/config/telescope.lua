447960681072178bc04c2b2f210ffb856d04e061
wheelibin <wheelibin@gmail.com>
2022-06-23 21:50:54 +0100

    Update dot_config/nvim/lua/config/telescope.lua

--- dot_config/nvim/lua/config/telescope.lua
+++ dot_config/nvim/lua/config/telescope.lua
@@ -1,15 +1,15 @@
-local map = vim.api.nvim_set_keymap
-local options = { noremap = true }
-
 local telescope = require('telescope')
-telescope.setup()
+local builtin = require("telescope.builtin")
 
+telescope.setup()
 telescope.load_extension('fzf')
 
--- find shortcuts
-map('n', '<Leader>ff', '<cmd>lua require("telescope.builtin").find_files()<cr>', options)
-map('n', '<Leader>fg', '<cmd>lua require("telescope.builtin").live_grep()<cr>', options)
-map('n', '<Leader>fb', '<cmd>lua require("telescope.builtin").buffers()<cr>', options)
-map('n', '<Leader>fh', '<cmd>lua require("telescope.builtin").help_tags()<cr>', options)
-map('n', '<Leader>fr', '<cmd>lua require("telescope.builtin").lsp_references({layout_strategy="vertical"})<cr>', options)
-map('n', '<Leader>fd', '<cmd>lua require("telescope.builtin").diagnostics({layout_strategy="vertical"})<cr>', options)
+local wk = require("which-key")
+wk.register({
+  ["<leader>f"] = { name = "+Find"},
+  ["<leader>ff"] = { builtin.find_files, "Files" },
+  ["<leader>fb"] = { builtin.buffers, "Buffers" },
+  ["<leader>fr"] = { function() builtin.lsp_references({layout_strategy="vertical"}) end, "References (LSP)" },
+  ["<leader>fd"] = { function() builtin.diagnostics({layout_strategy="vertical"}) end, "Diagnostics (LSP)" },
+})
+
