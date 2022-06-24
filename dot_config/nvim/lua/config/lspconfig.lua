8a1d1f71fdf7ca64b5cf5fbf349474b7c5d81af5
wheelibin <wheelibin@gmail.com>
2022-06-24 21:59:31 +0100

    Update dot_config/nvim/lua/config/lspconfig.lua

--- dot_config/nvim/lua/config/lspconfig.lua
+++ dot_config/nvim/lua/config/lspconfig.lua
@@ -1,34 +1,27 @@
 -- lspconfig
+local legendary = require('legendary')
+local helpers = require('legendary.helpers')
 
--- Mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
--- vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
--- vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+legendary.bind_keymaps({
+  -- { "[d",  '<cmd>lua vim.diagnostic.goto_prev()<CR>', description = "Prev Diagnostics Item", buffer = 0, mode = "t" }
+  { "[d", vim.diagnostic.goto_prev, description = "Prev Diagnostics message" },
+  { "]d", vim.diagnostic.goto_next, description = "Next Diagnostics message" }
+})
 
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
+
   -- Enable completion triggered by <c-x><c-o>
   vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-space>', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  -- vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>cf', '<cmd>lua vim.lsp.buf.format{ async = true }<CR>', opts)
+  legendary.bind_keymaps({
+    { "gD", vim.lsp.buf.declaration, description = "Go to declaration (LSP)", buffer = bufnr },
+    { "gd", vim.lsp.buf.definition, description = "Go to definition (LSP)", buffer = bufnr },
+    { "<C-space>", vim.lsp.buf.hover, description = "LSP Info", buffer = bufnr },
+    { "gi", vim.lsp.buf.implementation, description = "Go to implementation (LSP)", buffer = bufnr },
+    { "<leader>rn", vim.lsp.buf.rename, description = "Rename (LSP)", buffer = bufnr },
+    { "<leader>ca", vim.lsp.buf.code_action, description = "Code Action(s) (LSP)", buffer = bufnr },
+    { "<leader>f", helpers.lazy(vim.lsp.buf.format, { async = true }), description = "Format buffer (LSP)", buffer = bufnr }
+  })
 
   require('lsp_signature').on_attach()
   require('illuminate').on_attach(client)
