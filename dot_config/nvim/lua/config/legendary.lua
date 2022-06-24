e7ea70cb3429aa51d97bb06385f286814242a56d
Jon Wheeler <jon.wheeler@astrazeneca.com>
2022-06-24 11:37:19 +0100

    Add dot_config/nvim/lua/config/legendary.lua

---
+++ dot_config/nvim/lua/config/legendary.lua
@@ -0,0 +1,7 @@
+require('legendary').setup({
+  which_key = {
+      -- tell legendary that which-key will be doing the actual key bindings (false = leave it up to which-key)
+      do_binding = false,
+  },
+  -- include_builtin = false
+})
