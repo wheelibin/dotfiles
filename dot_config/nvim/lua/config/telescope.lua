0849ca3b5ab66f02a6d00f95bfd9137f040326aa
Jon Wheeler <jon.wheeler@astrazeneca.com>
2022-06-24 09:29:46 +0100

    Update dot_config/nvim/lua/config/telescope.lua

--- dot_config/nvim/lua/config/telescope.lua
+++ dot_config/nvim/lua/config/telescope.lua
@@ -8,6 +8,7 @@ local wk = require("which-key")
 wk.register({
   ["<leader>f"] = { name = "+Find"},
   ["<leader>ff"] = { builtin.find_files, "Files" },
+  ["<leader>fg"] = { builtin.live_grep, "Text (grep)"},
   ["<leader>fb"] = { builtin.buffers, "Buffers" },
   ["<leader>fr"] = { function() builtin.lsp_references({layout_strategy="vertical"}) end, "References (LSP)" },
   ["<leader>fd"] = { function() builtin.diagnostics({layout_strategy="vertical"}) end, "Diagnostics (LSP)" },
