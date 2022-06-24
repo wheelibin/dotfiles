195025edef2f18fc6367e55cfecba995346a8f6a
wheelibin <wheelibin@gmail.com>
2022-06-24 21:59:42 +0100

    Update dot_config/nvim/lua/config/toggleterm.lua

--- dot_config/nvim/lua/config/toggleterm.lua
+++ dot_config/nvim/lua/config/toggleterm.lua
@@ -12,27 +12,14 @@ require('toggleterm').setup({
   end
 })
 
+local legendary = require('legendary')
+
 function _G.set_terminal_keymaps()
-  local opts = {noremap = true}
-  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
-  -- vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
-  -- vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
-  -- vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
-  -- vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
+  legendary.bind_keymap({
+    "<esc>", [[<C-\><C-n>]], description = "Toggle Term", buffer = 0, mode = "t"
+  })
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
--- local map = require('utils').map
 
--- map('n', '<leader>t', ':ToggleTerm')
-
--- local Terminal  = require('toggleterm.terminal').Terminal
--- local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float"})
---
--- function Lazygit_toggle()
---   lazygit:toggle()
--- end
---
--- vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua Lazygit_toggle()<CR>", {noremap = true, silent = true})
