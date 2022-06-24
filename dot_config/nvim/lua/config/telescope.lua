8b8ae5bfc480da19796ee03ba720bc60ff11152e
Jon Wheeler <jon.wheeler@astrazeneca.com>
2022-06-24 11:39:32 +0100

    Update dot_config/nvim/lua/config/telescope.lua

--- dot_config/nvim/lua/config/telescope.lua
+++ dot_config/nvim/lua/config/telescope.lua
@@ -7,10 +7,10 @@ telescope.load_extension('fzf')
 local wk = require("which-key")
 wk.register({
   ["<leader>f"] = { name = "+Find"},
-  ["<leader>ff"] = { builtin.find_files, "Files" },
-  ["<leader>fg"] = { builtin.live_grep, "Text (grep)"},
-  ["<leader>fb"] = { builtin.buffers, "Buffers" },
-  ["<leader>fr"] = { function() builtin.lsp_references({layout_strategy="vertical"}) end, "References (LSP)" },
-  ["<leader>fd"] = { function() builtin.diagnostics({layout_strategy="vertical"}) end, "Diagnostics (LSP)" },
+  ["<leader>ff"] = { builtin.find_files, "Find Files" },
+  ["<leader>fg"] = { builtin.live_grep, "Find Text (grep)"},
+  ["<leader>fb"] = { builtin.buffers, "Find Buffers" },
+  ["<leader>fr"] = { function() builtin.lsp_references({layout_strategy="vertical"}) end, "Find References (LSP)" },
+  ["<leader>fd"] = { function() builtin.diagnostics({layout_strategy="vertical"}) end, "Find Diagnostics Items (LSP)" },
 })
 
