ac3fd2aacc55315b0e3ad377f7bd7fa6b6b9c0cd
Jon Wheeler <jon.wheeler@astrazeneca.com>
2022-06-29 10:52:40 +0100

    Update dot_config/nvim/lua/config/mini.lua

--- dot_config/nvim/lua/config/mini.lua
+++ dot_config/nvim/lua/config/mini.lua
@@ -1,8 +1,22 @@
-require('mini.comment').setup()
+local legendary = require('legendary')
 
+--
+-- enable individual modules
+--
+
+-- code commenting
+require('mini.comment').setup()
+legendary.bind_keymaps({
+  { 'gc', description = 'Toggle comment (like `gcip` - comment inner paragraph)', mode = { 'n', 'v' } },
+  { 'gcc', description = 'Toggle comment on current line' }
+})
+
+-- highlight word under cursor
 require('mini.cursorword').setup({ delay = 400 })
 vim.cmd('hi! link MiniCursorword Visual')
 
+-- auto pairs (braces, quotes etc)
 require('mini.pairs').setup()
 
+-- remove buffers retaining window layout
 require('mini.bufremove').setup()
