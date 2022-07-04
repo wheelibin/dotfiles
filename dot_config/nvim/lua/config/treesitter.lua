43c72131c5bce92c8c24d73485310ea4614d5894
Jon Wheeler <jon.wheeler@astrazeneca.com>
2022-07-04 14:49:38 +0100

    Update dot_config/nvim/lua/config/treesitter.lua

--- dot_config/nvim/lua/config/treesitter.lua
+++ dot_config/nvim/lua/config/treesitter.lua
@@ -1,6 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { 'lua', 'typescript', 'javascript', 'html' },
+  ensure_installed = { 'lua', 'typescript', 'tsx', 'javascript', 'html' },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
