ed242a3fccff7e2d6413b2c258995296e81eae86
Jon Wheeler <jon.wheeler@astrazeneca.com>
2022-07-06 08:07:06 +0100

    Add dot_config/nvim/lua/config/gitsigns.lua Update dot_config/nvim/lua/config/nvim-cmp.lua

--- dot_config/nvim/lua/config/nvim-cmp.lua
+++ dot_config/nvim/lua/config/nvim-cmp.lua
@@ -7,14 +7,6 @@ local feedkey = function(key, mode)
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
 end
 
-
--- require('lsp_signature').setup({
---   bind = true, -- This is mandatory, otherwise border config won't get registered.
---   handler_opts = {
---     border = "rounded"
---   }
--- })
-
 local cmp = require('cmp')
 cmp.setup {
 
@@ -33,6 +25,14 @@ cmp.setup {
   },
 
   enabled = function()
+
+    local buftype = vim.api.nvim_buf_get_option(0, "buftype")
+
+    -- prevents completion in prompts such as telescope/legendary
+    if buftype == "prompt" then
+      return false
+    end
+
     -- disable completion in comments
     local context = require 'cmp.config.context'
     -- keep command mode completion enabled when cursor is in a comment
