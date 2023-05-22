03d259fd30132f5918bc2b0c41894688d6092d53
Jon Wheeler <jon.wheeler@astrazeneca.com>
2023-05-22 16:47:05 +0100

    Remove dot_config/nvim/lua/config/coc.lua Remove dot_config/nvim/lua/config/coq.lua Remove dot_config/nvim/lua/config/fugitive.lua Remove dot_config/nvim/lua/config/gitsigns.lua Remove dot_config/nvim/lua/config/illuminate.lua Remove dot_config/nvim/lua/config/leap.lua Remove dot_config/nvim/lua/config/lspconfig.lua Remove dot_config/nvim/lua/config/lualine.bak Remove dot_config/nvim/lua/config/lualine.lua Remove dot_config/nvim/lua/config/mini.lua Remove dot_config/nvim/lua/config/neo-tree.lua Remove dot_config/nvim/lua/config/neogit.lua Remove dot_config/nvim/lua/config/neotest.lua Remove dot_config/nvim/lua/config/nord.lua Remove dot_config/nvim/lua/config/null-ls.lua Remove dot_config/nvim/lua/config/nvim-cmp.lua Remove dot_config/nvim/lua/config/nvim-notify-spinner.lua Remove dot_config/nvim/lua/config/nvim-notify.lua Remove dot_config/nvim/lua/config/nvim-tree.lua Remove dot_config/nvim/lua/config/qmk.lua Remove dot_config/nvim/lua/config/telescope.lua Remove dot_config/nvim/lua/config/toggleterm.lua Remove dot_config/nvim/lua/config/treesitter.lua Remove dot_config/nvim/lua/config/typescript.lua

--- dot_config/nvim/lua/config/leap.lua
+++
@@ -1,5 +0,0 @@
-require('leap').add_default_mappings()
-
-local legendary = require('legendary')
-legendary.keymap({ 'f', '<Plug>(leap-forward-to)', description = 'Leap (forward)' })
-legendary.keymap({ 'F', '<Plug>(leap-backward-to)', description = 'Leap (backward)' })
--- dot_config/nvim/lua/config/neo-tree.lua
+++
@@ -1,6 +0,0 @@
-local legendary = require('legendary')
-legendary.keymap({
-  '<leader>n',
-  [[:Neotree source=filesystem reveal=true position=left toggle=true<cr>]],
-  description = 'Nvim-Tree Toggle'
-})
