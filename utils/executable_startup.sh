5649cfd14a24085adca5cb4c9d74c6ee7b31b431
Jon Wheeler <wheelibin@gmail.com>
2023-05-23 21:25:59 +0100

    Update utils/startup.sh

--- utils/executable_startup.sh
+++ utils/executable_startup.sh
@@ -1,10 +1,10 @@
 # enable window transparency and shadows
-picom --config ~/.config/picom/picom.conf -b -c
+# picom --config ~/.config/picom/picom.conf -b
 
 # caps lock as escape and ctrl
 # xcape -e '#66=Escape' -t 100
 
 # set random wallpaper
-~/utils/wallpaper-change.sh
+# ~/utils/wallpaper-change.sh
 
 redshift -c ~/.config/redshift.conf
