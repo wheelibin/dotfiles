83f03a017574e5291ece3cb38cebcdf69bd469e7
Jon Wheeler <jon.wheeler@astrazeneca.com>
2023-06-30 21:17:50 +0100

    Update dot_config/nvim/lua/plugins/_theme.lua Add dot_config/nvim/lua/plugins/dap.lua Update dot_config/nvim/lua/plugins/lsp.lua

--- dot_config/nvim/lua/plugins/_theme.lua
+++ dot_config/nvim/lua/plugins/_theme.lua
@@ -44,7 +44,7 @@
 return {
   'EdenEast/nightfox.nvim',
   config = function()
-    vim.cmd('colorscheme terafox')
+    vim.cmd('colorscheme nightfox')
   end
 
 }
--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -25,7 +25,7 @@ return {
         'L3MON4D3/LuaSnip',
         dependencies = { "rafamadriz/friendly-snippets" },
       },
-      { 'saadparwaiz1/cmp_luasnip' }
+      { 'saadparwaiz1/cmp_luasnip' },
     },
     config = function()
       local servers = {
