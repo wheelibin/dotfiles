45f15fd473a4a38358f08b9663ae832475cedc14
Jon Wheeler <wheelibin@gmail.com>
2023-10-11 08:45:06 +0100

    Delete dot_config/nvim/lua/plugins/smart-splits.lua

--- dot_config/nvim/lua/plugins/smart-splits.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'mrjones2014/smart-splits.nvim',
-  config = function()
-    vim.keymap.set('n', '<M-m>', require('smart-splits').move_cursor_left)
-    vim.keymap.set('n', '<M-n>', require('smart-splits').move_cursor_down)
-    vim.keymap.set('n', '<M-e>', require('smart-splits').move_cursor_up)
-    vim.keymap.set('n', '<M-i>', require('smart-splits').move_cursor_right)
-  end
-}
