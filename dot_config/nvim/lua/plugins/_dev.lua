17cbc29d5ea984edd197d8db8e314f58454e5ba4
wheelibin <wheelibin@gmail.com>
2023-10-15 13:08:06 +0100

    Update .config/nvim/lua/plugins/_dev.lua

--- dot_config/nvim/lua/plugins/_dev.lua
+++ dot_config/nvim/lua/plugins/_dev.lua
@@ -290,36 +290,59 @@ return {
   },
 
   {
-    'mfussenegger/nvim-dap',
+    'rcarriga/nvim-dap-ui',
+    lazy = true,
     dependencies = {
-      { 'leoluz/nvim-dap-go' }
+      { 'leoluz/nvim-dap-go' },
+      { 'theHamsta/nvim-dap-virtual-text' },
+      {
+        'mfussenegger/nvim-dap',
+        config = function()
+          require('dap-go').setup()
+        end
+      }
     },
+    keys = {
+      -- { '<Leader>d',  function() require("dapui").toggle() end,          desc = "DAP continue" },
+      { '<Leader>b',  function() require('dap').toggle_breakpoint() end, desc = "DAP continue" },
+      { '<Leader>dr', function() require('dap').repl.open() end,         desc = "DAP repl open" },
+      { '<F5>',       function() require('dap').continue() end,          desc = "DAP continue" },
+      { '<F10>',      function() require('dap').step_over() end,         desc = "DAP step over" },
+      { '<F11>',      function() require('dap').step_into() end,         desc = "DAP step into" },
+      { '<F12>',      function() require('dap').step_out() end,          desc = "DAP step out" },
+    },
+
     config = function()
-      require('dap-go').setup()
-      vim.keymap.set('n', '<F5>', function() require('dap').continue() end)
-      vim.keymap.set('n', '<F10>', function() require('dap').step_over() end)
-      vim.keymap.set('n', '<F11>', function() require('dap').step_into() end)
-      vim.keymap.set('n', '<F12>', function() require('dap').step_out() end)
-      vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
-      vim.keymap.set('n', '<Leader>B', function() require('dap').set_breakpoint() end)
-      vim.keymap.set('n', '<Leader>lp',
-        function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
-      vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
-      vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
-      vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
-        require('dap.ui.widgets').hover()
-      end)
-      vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
-        require('dap.ui.widgets').preview()
-      end)
-      vim.keymap.set('n', '<Leader>df', function()
-        local widgets = require('dap.ui.widgets')
-        widgets.centered_float(widgets.frames)
-      end)
-      vim.keymap.set('n', '<Leader>ds', function()
-        local widgets = require('dap.ui.widgets')
-        widgets.centered_float(widgets.scopes)
-      end)
+      require("dapui").setup()
+      local dap, dapui = require("dap"), require("dapui")
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+
+      -- vim.keymap.set('n', '<Leader>lp',
+      --   function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
+      -- vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
+      -- vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
+      -- vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
+      --   require('dap.ui.widgets').hover()
+      -- end)
+      -- vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
+      --   require('dap.ui.widgets').preview()
+      -- end)
+      -- vim.keymap.set('n', '<Leader>df', function()
+      --   local widgets = require('dap.ui.widgets')
+      --   widgets.centered_float(widgets.frames)
+      -- end)
+      -- vim.keymap.set('n', '<Leader>ds', function()
+      --   local widgets = require('dap.ui.widgets')
+      --   widgets.centered_float(widgets.scopes)
+      -- end)
     end
   }
 
