ba43403bf0fecec9be399f87f266a96926ce91c9
wheelibin <wheelibin@gmail.com>
2023-10-16 16:44:28 +0100

    Update .config/nvim/lua/plugins/_dev.lua

--- dot_config/nvim/lua/plugins/_dev.lua
+++ dot_config/nvim/lua/plugins/_dev.lua
@@ -308,7 +308,6 @@ return {
       }
     },
     keys = {
-      -- { '<Leader>d',  function() require("dapui").toggle() end,          desc = "DAP continue" },
       { '<Leader>b',  function() require('dap').toggle_breakpoint() end, desc = "DAP continue" },
       { '<Leader>dr', function() require('dap').repl.open() end,         desc = "DAP repl open" },
       { '<F5>',       function() require('dap').continue() end,          desc = "DAP continue" },
@@ -329,25 +328,6 @@ return {
       dap.listeners.before.event_exited["dapui_config"] = function()
         dapui.close()
       end
-
-      -- vim.keymap.set('n', '<Leader>lp',
-      --   function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
-      -- vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
-      -- vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
-      -- vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
-      --   require('dap.ui.widgets').hover()
-      -- end)
-      -- vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
-      --   require('dap.ui.widgets').preview()
-      -- end)
-      -- vim.keymap.set('n', '<Leader>df', function()
-      --   local widgets = require('dap.ui.widgets')
-      --   widgets.centered_float(widgets.frames)
-      -- end)
-      -- vim.keymap.set('n', '<Leader>ds', function()
-      --   local widgets = require('dap.ui.widgets')
-      --   widgets.centered_float(widgets.scopes)
-      -- end)
     end
   }
 
