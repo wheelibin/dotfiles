3c2b9beaf7b19c51c980eef82eb73a8ba3d1af39
wheelibin <wheelibin@gmail.com>
2024-02-07 07:34:28 +0000

    Update .config/nvim/lua/plugins/_editor.lua

--- dot_config/nvim/lua/plugins/_editor.lua
+++ dot_config/nvim/lua/plugins/_editor.lua
@@ -151,7 +151,7 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     lazy = true,
-    branch = "v2.x",
+    branch = "v3.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
