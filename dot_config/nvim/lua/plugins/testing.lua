52959f20d7f34f7dff0d0be41df1020c8a9875c6
Jon Wheeler <jon.wheeler@astrazeneca.com>
2025-03-17 10:46:03 +0000

    Update .config/nvim/lua/plugins/debug.lua Update .config/nvim/lua/plugins/fzf.lua Update .config/nvim/lua/plugins/testing.lua

--- dot_config/nvim/lua/plugins/testing.lua
+++ dot_config/nvim/lua/plugins/testing.lua
@@ -12,15 +12,25 @@ return {
         version = "*",
         dir = "~/dev/neotest-golang"
       }, -- Installation
+      "nvim-neotest/neotest-jest"
     },
     keys = {
       { "<leader>tn", "<cmd>lua require('neotest').run.run()<CR>",                   mode = { 'n' } },
+      { "<leader>td", "<cmd>lua require('neotest').run.run({strategy = 'dap'})<CR>", mode = { 'n' } },
       { "<leader>tf", "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<CR>", mode = { 'n' } },
     },
     config = function()
       require("neotest").setup({
         adapters = {
           require("neotest-golang"), -- Registration
+          require('neotest-jest')({
+            -- jestCommand = "npm test --",
+            -- jestConfigFile = "custom.jest.config.ts",
+            -- env = { CI = true },
+            -- cwd = function(path)
+            --   return vim.fn.getcwd()
+            -- end,
+          }),
         },
       })
     end,
